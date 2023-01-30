338bc292f279f451b8e5e5227167b2c1230caada
Anton Scherbakov <bioiiiokolat@gmail.com>
2023-01-30 11:36:03 +0300

    Init

---
+++ src/app/models/file.ts
@@ -0,0 +1,8 @@
+export class File {
+    public lastModified: String | undefined;
+    public lastModifiedDate: Date | undefined;
+    public name: String | undefined;
+    public size: Number | undefined;
+    public type: String | undefined;
+    public webkitRelativePath: String | undefined;
+}
